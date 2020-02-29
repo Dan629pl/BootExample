3c0b11eb81e67e56ec5ec28c8aa5fb6a27fcd32b
Daniel <daniel.owczarczyk.pl@gmail.com>
2020-02-29 19:53:12 +0100

    Usuniecie zbednego sout

--- src/main/java/org/example/algorithms/AlgorithmImpl.java
+++ src/main/java/org/example/algorithms/AlgorithmImpl.java
@@ -1,10 +1,9 @@
 package org.example.algorithms;
 
 public class AlgorithmImpl implements Algorithm {
-
+    
     @Override
     public int getMinimumNumberOfCubeMoves(int[] values) {
-        System.out.println(values);
         return 0;
     }
 }
